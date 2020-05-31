54c4f6fa777429443e126af6d8bdab7e0e9cbbdb
Holly Prothe <hollyprothe1@gmail.com>
2020-05-31 16:20:26 -0500

    add routes to htmlRoutes for notes.html and index.html

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Dependencies:
+
+const path = require("path");
+const router = require("express").Router();
+
+//Routes:
+
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/notes.html"));
+});
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/index.html"));
+});
+
+module.exports = router;
